d104e645c2dd1fa0111ef67f5986ed2333e47080
Alvaro <alvaro.delarocharamos@gmail.com>
2021-03-21 17:15:14 -0700

    Ventanas de inicio de sesion y demas formularios

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,6 @@
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Order
 
 
@@ -7,4 +9,9 @@ class OrderForm(ModelForm):
         model = Order
         fields = '__all__'
         # si solo necesitas uno: "['customer', ]"
-        
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
